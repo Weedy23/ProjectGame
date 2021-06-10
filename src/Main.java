8648f0652ddc644ec74e367d000d0896e72ce150
Weedy <dkeziks@yahoo.com>
2021-06-10 14:11:51 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     private static final MakeCharacter MakeCharacter = new MakeCharacter();
     private static final Game Game = new Game();
     protected static character Player = new character();
+    private static final LoadSave LOAD_SAVE = new LoadSave();
+    static final String PATHcharacter = "C:\\Progmeistars\\ProjectGame\\Saves\\Character.txt";
 
-    public static void main(String[] args) {
-        Player = MakeCharacter.MakeCharacter();
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(PATHcharacter);
+        if (LOAD_SAVE.FileCheck(path)) {
+            MakeCharacter.MakeCharacter();
+        } else {
+            LOAD_SAVE.LoadCharacter(path);
+        }
         Game.Game();
     }
 }
