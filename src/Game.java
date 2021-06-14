a85680e6f9b6db78d3461c93684e345e2d6f96c1
Weedy <dkeziks@yahoo.com>
2021-06-14 13:48:10 +0300

    Game process

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Game extends Main{
     private static final Tower Tower = new Tower();
     private static final World World = new World();
     private static final Window win = new Window();
+    private static final LoadSave LOAD_SAVE = new LoadSave();
     private static String Place = "World";
-    private Game game = new Game();
 
-    public void Game() {
-        win.Print("Hi, " + game.Player.getCharName() + ", now you are in " + Place + "!");
-
+    public void Game() throws IOException {
+        win.Print("Hi, " + Player.getCharName() + ", now you are in " + Place + "!");
+        Path path = Paths.get(PATHcharacter);
+        LOAD_SAVE.SaveCharacter(path);
+        /*
          switch (Place) {
              case "World":
                  WorldProcess();
              case "Tower":
                  TowerProcess();
          }
+
+         */
     }
 }
