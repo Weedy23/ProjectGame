a574521cdd46964cebe2ef7ff84c242570fdc0ef
Weedy <dkeziks@yahoo.com>
2021-06-19 00:31:30 +0300

    Game process

--- src/Game.java
+++ src/Game.java
@@ -2,25 +2,153 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Game extends Main{
-    private static final Tower Tower = new Tower();
-    private static final World World = new World();
+public class Game extends Main {
     private static final Window win = new Window();
     private static final LoadSave LOAD_SAVE = new LoadSave();
-    private static String Place = "World";
+    private static String time = "Day";
 
     public void Game() throws IOException {
-        win.Print("Hi, " + Player.getCharName() + ", now you are in " + Place + "!");
-        Path path = Paths.get(PATHcharacter);
-        LOAD_SAVE.SaveCharacter(path);
-        /*
-         switch (Place) {
-             case "World":
-                 WorldProcess();
-             case "Tower":
-                 TowerProcess();
-         }
+        LOAD_SAVE.SaveCharacter(Paths.get(PATHcharacter));
 
-         */
+        win.Print("Hi, " + Player.getCharName() + " Now is " + time);
+
+        int timeCounter = 0;
+        for ( ; ; ) {
+            Move();
+            timeCounter++;
+            LOAD_SAVE.SaveCharacter(Paths.get(PATHcharacter));
+
+            if (timeCounter == 3) {
+                timeCounter = 0;
+                switch (time) {
+                    case "Day":
+                        time = "Night";
+                        break;
+                    case "Night":
+                        time = "Day";
+                        break;
+                }
+                win.Print("Time switched to " + time);
+                win.Print("");
+            }
+        }
+    }
+
+    public void Move() throws IOException {
+        int n = (int) (Math.random() * 10);
+        int N = 0;
+        switch (time) {
+            case "Day":
+                N = 5;
+                break;
+            case "Night":
+                N = 3;
+                break;
+        }
+        if (n > N) {
+            Loot loot = Loot.generateLootOrNothing(Player.getLevel());
+            if (loot != null) {
+                win.Print("You are lucky! you got " + loot.getName() + "!");
+                win.Print("New loot stats: " + loot.viewLootStat());
+                if (Player.getCharLoot() != null) {
+                    win.Print("Your loot stats:" + Player.getCharLoot().viewLootStat());
+                } else {
+                    win.Print("You don't have loot equipped");
+                }
+                win.Print("Do you want to change your loot& Yes or No!");
+
+                switch (win.getText()) {
+                    case "Yes":
+                        if (Player.getCharLoot() == null) {
+                            Player.getCharStats().setMaxHp(Player.getCharStats().getMaxHp() + loot.getHp());
+                            Player.getCharStats().setHp(Player.getCharStats().getMaxHp() + loot.getHp());
+                            Player.getCharStats().setDefense(Player.getCharStats().getDefense() + loot.getDefense());
+                        } else {
+                            Player.getCharStats().setMaxHp(Player.getCharStats().getMaxHp() - Player.getCharLoot().getHp() + loot.getHp());
+                            Player.getCharStats().setHp(Player.getCharStats().getMaxHp() - Player.getCharLoot().getHp() + loot.getHp());
+                            Player.getCharStats().setDefense(Player.getCharStats().getDefense() - Player.getCharLoot().getDefense() + loot.getDefense());
+                        }
+                        Player.setCharLoot(loot);
+                        break;
+                    case "No":
+                        win.Print("Ok");
+                        break;
+                }
+            } else {
+                win.Print("You got nothing");
+            }
+        } else {
+            Enemies enemies = Enemies.generateEnemiesOrNothing(Player.getLevel());
+            if (enemies != null) {
+                win.Print("You meet " + enemies.getName());
+                win.Print("FIGHT!");
+                enemies = fight(enemies);
+            } else {
+                win.Print("You are lucky! you don't have to fight");
+            }
+        }
+    }
+
+    public static Enemies fight(Enemies enemies) throws IOException {
+        for ( ; Player.getCharStats().getHp() > 0 && enemies.getHp() > 0; ) {
+            win.Print("Your hp " + Player.getCharStats().getHp());
+            win.Print("Your stamina " + Player.getCharStats().getStamina());
+            win.Print("Your Intelligence " + Player.getCharStats().getIntelligence());
+            win.Print("enemies hp " + enemies.getHp());
+            win.Print("");
+            win.Print("Choose attack type");
+            win.Print("BaseAttack, StrongAttack, BaseAbility, SpecialAbility, Heal");
+            switch (win.getText()) {
+                case "BaseAttack":
+                    enemies.setHp(enemies.getHp() - MakeAttack(Player.MakeAttack("BaseAttack"), Player.getCharStats().getCriticalChance(), Player.getCharStats().getCriticalPower(), enemies.getDodge(), enemies.getDefense()));
+                    break;
+                case "StrongAttack":
+                    enemies.setHp(enemies.getHp() - MakeAttack(Player.MakeAttack("StrongAttack"), Player.getCharStats().getCriticalChance(), Player.getCharStats().getCriticalPower(), enemies.getDodge(), enemies.getDefense()));
+                    break;
+                case "BaseAbility":
+                    enemies.setHp(enemies.getHp() - MakeAttack(Player.MakeAttack("BaseAbility"), Player.getCharStats().getCriticalChance(), Player.getCharStats().getCriticalPower(), enemies.getDodge(), enemies.getDefense()));
+                    break;
+                case "SpecialAbility":
+                    enemies.setHp(enemies.getHp() - MakeAttack(Player.MakeAttack("SpecialAbility"), Player.getCharStats().getCriticalChance(), Player.getCharStats().getCriticalPower(), enemies.getDodge(), enemies.getDefense()));
+                    break;
+                case "Heal":
+                    Player.getCharStats().setHp(MakeHeal(Player.getCharStats().getHp(), Player.getCharStats().getMaxHp(), Player.MakeAttack("Heal")));
+                    break;
+            }
+
+            Player.getCharStats().setHp(Player.getCharStats().getHp() - MakeAttack(enemies.getAttack(), 0, 1, 20, Player.getCharStats().getDefense()));
+
+        }
+        if (enemies.getHp() <= 0) {
+            win.Print("You won");
+            Player.setExperience(Player.getExperience() + enemies.getExperience());
+            Player.LevelUp();
+        } else {
+            win.Print("You lose");
+            LOAD_SAVE.FileDelete(Paths.get(PATHcharacter));
+        }
+        return null;
+    }
+
+    public static int MakeAttack(int AttackDamageAttacker, int CriticalChanceAttacker, int CriticalPowerAttacker, int DodgeChanceDefender, int DefensePowerDefender) {
+        int Dodge = (int) (Math.random() * 100) + 1;
+        if (Dodge <= DodgeChanceDefender) {
+            int Critical = (int) (Math.random() * 100) + 1;
+            if (Critical <= CriticalChanceAttacker) {
+                return (AttackDamageAttacker - DefensePowerDefender) * CriticalPowerAttacker;
+            } else {
+                return AttackDamageAttacker - DefensePowerDefender;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    public static int MakeHeal(int Hp, int MaxHp, int Heal) {
+        if (Hp + Heal <= MaxHp) {
+            return Hp + Heal;
+        } else {
+            return MaxHp;
+        }
     }
 }
